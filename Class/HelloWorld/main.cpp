632854605c8fed37459a91dabc1ea48f717b91c7
OvAlexander <OvAlexander@users.noreply.github.com>
2020-01-20 22:35:25 -0800

    file changes

---
+++ Class/HelloWorld/main.cpp
@@ -0,0 +1,45 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on January 6, 2020, 12:40 PM
+ * Purpose:  First Program "Hello World"
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - No Global Variables
+//Only Universal Constants, Math, Physics, Conversions, Higher Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Set Random Number seed
+    
+    //Declare Variable Data Types and Constants
+    
+    //Initialize Variables
+    
+    //Process or map Inputs to Outputs
+    
+    //Display Outputs
+    cout<<"Hello World"<<endl;//First Line Output
+    
+    cout<<"Hello ";
+    cout<<"World\n";//Second Line Output
+    
+    cout<<"Hello ";
+    cout<<"World";
+    cout<<"\n";//Third Line Output
+    
+    cout<<"Hello ";
+    cout<<"World";
+    cout<<endl;//Fourth Line Output
+    
+    //Exit stage right!
+    return 0;
+}
--- Class/README.md
+++
@@ -1,2 +0,0 @@
-# 2020_Winter_CSC_CIS_5
-RCC Winter CSC/CIS 5 - Introduction to Programming:  C++
